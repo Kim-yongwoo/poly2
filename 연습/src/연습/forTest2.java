8c28ec9750bd0ad2c797cd745d539cc3bd78dc28
김용우 <@DESKTOP-RSNNA9E>
2021-03-10 16:24:23 +0900

    나의 두번째 프로그래밍 for문 연습을 Git에 올리기

---
+++ 연습/src/연습/forTest2.java
@@ -0,0 +1,14 @@
+package 연습;
+
+public class forTest2 {
+
+	public static void main(String[] args) {
+		int a; // 변수를 a로 하든 hap으로 하던 상관x
+		
+		a = 1+2+3+4+5+6+7+8+9+10;
+		
+		System.out.printf("1에서 10까지의 합: %d \n", a);
+
+	}
+
+}
