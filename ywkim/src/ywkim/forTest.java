db297ee1bd825a5a785a5a67a3a7877735403881
김용우 <@DESKTOP-RSNNA9E>
2021-03-17 16:04:42 +0900

    first git

---
+++ ywkim/src/ywkim/forTest.java
@@ -0,0 +1,14 @@
+package ywkim;
+
+public class forTest {
+
+	public static void main(String[] args) {
+		int i;
+		
+		for (i=5; i>0; i--) {
+			System.out.printf("%d for문 연습중... \n", i);
+		}
+
+	}
+
+}
